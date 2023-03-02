02f0383b5353b9a184b8725ec2476f4153211009
abdelilah Fakhour <abdelilah.fakhour@outlook.fr>
2023-03-02 14:03:17 +0100

    retrait code

--- Exo 4/app.js
+++ Exo 4/app.js
@@ -34,30 +34,3 @@ const setBg = (item) => { // fonction générant une couleur aléatoire
     }
     item.style.backgroundColor = "#" + randomColor; // le # plus les chiffre hexa donne le code couleur
 }
-
-// const setBg2 = (item) => { // fonction générant une couleur aléatoire en jouant sur la transparence
-//     let randomColor = "";
-//     while (randomColor.length != 8) { // le chiffre correspond au code couleur #000000|00| code pour la transparence
-//         randomColor = Math.floor(Math.random() * 4294967295).toString(16);
-//         console.log(randomColor);
-//     }
-//     item.style.backgroundColor = "#" + randomColor; // le # plus les chiffre hexa donne le code couleur css
-// }
-
-
-// autre fonction couleur aléatoire à confirmer
-// function randomColor() {
-//     var x = Math.floor(Math.random() * 256);
-//     var y = Math.floor(Math.random() * 256);
-//     var z = Math.floor(Math.random() * 256);
-//     var bgColor = x + y + z;
-
-//     carre.style.background = bgColor;
-// }
-
-// const randomRgbColor = () => {
-//     const r = Math.floor(Math.random() * 256);
-//     const g = Math.floor(Math.random() * 256);
-//     const b = Math.floor(Math.random() * 256);
-//     return `rgb(${r}, ${g}, ${b})`;
-//   };
